9deea41a810ed0066be65d9a6608d0b8e4fc495e
kickscar <kickscar@gmail.com>
2021-05-24 13:28:41 +0900

    lexical structure #1

---
+++ ch02/03/ex01.js
@@ -0,0 +1,6 @@
+/* Literal Example */
+
+let i = 10;
+let j = 20;
+let s1 = 'hello';
+let s2 = "JavaScript is Awesome";
---
+++ ch02/03/ex02.js
@@ -0,0 +1,5 @@
+/* 리터럴과 식별자의 구분 */
+
+caffeAmericano = 4100;
+orderPrice = caffeAmericano;
+orderMenu = "caffeAmericano";
