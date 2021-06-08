7b19c68ad52edbd78ab173e68acbae0d7262e1d5
kickscar <kickscar@gmail.com>
2021-06-08 19:31:23 +0900

    Execution Context #2

--- ch02/07/ex01.js
+++ ch02/07/ex01.js
@@ -1,16 +1,16 @@
-
 console.log('Global EC pushed');
 
-const f1 = function() {
+function f1() {
     console.log("f1 Function EC pushed");
+
+    const f2 = function() {
+        console.log("f2 Function EC pushed");
+        // some codes...
+        console.log('f2 Function EC popped off');
+    }
+    
     f2();
     console.log('f1 Function EC popped off');
-}
-
-const f2 = function() {
-    console.log("f2 Function EC pushed");
-    // some codes...
-    console.log('f2 Function EC popped off');
 }
 
 f1();
